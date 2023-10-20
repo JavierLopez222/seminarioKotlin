76c51f5ed8825add3f54337fd8e605a446beb17f
javil <javilopezguadix03@gmail.com>
2023-10-20 22:03:33 +0200

    Commit ejercicios resueltos 24-30

--- app/src/main/java/com/example/seminariokotlin/ejercicio10.kt
+++ app/src/main/java/com/example/seminariokotlin/ejercicio10.kt
@@ -1,3 +1,24 @@
 package com.example.seminariokotlin
 
 //Crea una función que calcule el término n-ésimo de la sucesión de Finbonacci.
+
+fun crearFibonacci(n: Int): Long{
+    if (n <= 1){
+        return n.toLong()
+    }
+    var prev = 0L
+    var current = 1L
+
+    for(i in 2..n){
+        var next = prev + current
+        prev = current
+        current = next
+    }
+    return current
+}
+
+fun main(){
+    var n = 10
+    var resultado = crearFibonacci(n)
+    println("El término $n de la sucesión de Fibonacci es $resultado")
+}
--- app/src/main/java/com/example/seminariokotlin/ejercicio13.kt
+++ app/src/main/java/com/example/seminariokotlin/ejercicio13.kt
@@ -2,3 +2,20 @@ package com.example.seminariokotlin
 
 //Crea una función que dada una cadena de texto con formato Emmet devuelva su correspondiente etiqueta HTML,
 // teniendo en cuenta sólo los atributos de clase e id.
+
+
+fun emmetToHtml(emmet: String): String {
+    val tokens = emmet.split(" ")
+    var html = ""
+    for (token in tokens) {
+        if (token.startsWith(".") || token.startsWith("#")) {
+            html += " $token"
+        }
+    }
+    return html
+}
+
+
+fun main() {
+
+}
